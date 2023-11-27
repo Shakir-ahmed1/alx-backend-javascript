81e4de7c427af59066bf326595cdfcba20d3425d
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-11-27 19:04:06 +0000

    using only let

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    task2 = true;
-    task = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
