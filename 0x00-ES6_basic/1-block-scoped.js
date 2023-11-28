c6a1f27251634d8a65ca7afeb12aea25fa114097
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-11-28 11:33:59 +0000

    empty if condition

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,6 +2,8 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  trueOrFalse;
+  // eslint-disable-next-line no-empty
+  if (trueOrFalse) {
+  }
   return [task, task2];
 }
