e2d22e640888207380955d9c6ee58bec25ea1d49
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-11-29 22:55:12 +0000

    Await/Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,19 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function () {
+  let obj;
+  try {
+    const user = await uploadPhoto();
+    const photo = await createUser();
+    obj = {
+      user,
+      photo,
+    };
+  } catch (e) {
+    obj = {
+      phto: null,
+      user: null,
+    };
+  }
+  return obj;
+}
