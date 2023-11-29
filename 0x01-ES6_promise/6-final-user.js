632445208c9f9076ffb014aef88a605b2303d758
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-11-29 23:43:51 +0000

    renaming parametres

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,9 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
-  const photo = uploadPhoto(filename);
+  const photo = uploadPhoto(fileName);
   const promises = [user, photo];
   return Promise.allSettled(promises).then((values) => {
     const list = [
