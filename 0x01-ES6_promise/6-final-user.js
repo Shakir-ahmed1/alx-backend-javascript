57a68333af9e4c1774edd4f601389d8d07483cbd
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-11-29 20:38:36 +0000

    handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,18 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(filename);
   const promises = [user, photo];
-  return Promise.allSettled(promises);
+  return Promise.allSettled(promises).then((values) => {
+    const list = [
+      {
+        status: values[0].status,
+        value: values[0].status === 'fulfilled' ? values[0].value : values[0].reason,
+      },
+      {
+        status: values[1].status,
+        value: values[1].status === 'fulfilled' ? values[1].value : values[1].reason,
+      },
+    ];
+    return list;
+  });
 }
+
