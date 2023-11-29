179ed96f644a53e765c2a427e11a10566e8f8796
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-11-29 18:24:04 +0000

    submit code

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => {
-    console.log('Got a response from the API');
     const obj = {
       status: 200,
       body: 'success',
     };
     return obj;
-  }, () => {
-    return new Error();
+  }).catch(() => new Error()).finally(() => {
+    console.log('Got a response from the API');
   });
 }
