3f0bf85f0100bc6c9ea1ed3bf407ec9c986e4c2c
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-11-30 00:40:06 +0000

    iterationg through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,9 @@
 export default function createIteratorObject(report) {
-  return ['Bob', 'Jane', 'Sylvie'];
+  let result = [];
+  for (const x in report.allEmployees) {
+    if (x) {
+      result = result.concat(report.allEmployees[x]);
+    }
+  }
+  return result;
 }
