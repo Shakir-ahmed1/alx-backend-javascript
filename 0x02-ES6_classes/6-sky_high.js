9114123804a92af908ead62f814d5e2227441c78
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-11-30 22:45:26 +0000

    Sky High

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,18 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors !== 'number') throw Error('Floors must be a number');
+    this._floor = floors;
+  }
+
+  get floors() {
+    return this._floor;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    return 'Evacuate slowly the NUMBER_OF_FLOORS floors';
+  }
+}
