3447d2c726873b84c7e8adfec9bd769c61348dd9
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-05 16:57:08 +0000

    adding out of range error

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,9 @@
 export default function createInt8TypedArray (length, position, value) {
   const buff = new ArrayBuffer(length);
-  const buffview = new DataView(buff)
+  const buffview = new DataView(buff);
+  if (position >= length) {
+     throw new Error('Position outside range');
+  }
   buffview.setInt8(position,value);
 
   return buffview;
