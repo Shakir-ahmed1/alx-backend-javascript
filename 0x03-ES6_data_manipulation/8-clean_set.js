500251d520d57c5dbf7950a95346f58dabbffca0
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-05 18:56:07 +0000

    fixing correct output 2

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,8 @@
 export default function cleanSet(set, startString) {
   const result = [];
+  if (startString === '' || startString.length === 0) {
+    return '';
+  }
   for (const element of set) {
     if (element.startsWith(startString)) {
       // result.push(element.slice(startString.length));
