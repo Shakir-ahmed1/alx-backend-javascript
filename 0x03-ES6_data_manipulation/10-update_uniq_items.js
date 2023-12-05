904b8ea748ea651bc258b1cb59712a9decba1969
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-05 18:26:40 +0000

    update unique items

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,11 @@
 export default function updateUniqueItems(elements) {
   const gr = elements;
-  if (!elements instanceof Map) {
+  if (!(elements instanceof Map)) {
     throw new Error('Cannot process');
   }
-  for (const element of gr.keys()) {      
-    if (gr.get(element) == 1) {
-      gr.set(element,100);
+  for (const element of gr.keys()) {
+    if (gr.get(element) === 1) {
+      gr.set(element, 100);
     }
   }
   return gr;
