539dd88dd3f7163b4c1b99113f40b9dce44ee20c
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-05 16:19:18 +0000

    filtering and mapping

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,16 @@
 export default function updateStudentGradeByCity(studentsList, city, newGrades) {
   return studentsList
     .filter((value) => value.location === city)
-    .map((value) => {
-	    for (const val of newGrades) {
-	      if (value.id === val.studentId) {
-	        value.grade = val.grade;
-	      }
-	    }
-	    if (typeof value.grade === 'undefined') {
-	      value.grade = 'N/A';
-	    }
-	    return value;
+    .map((vl) => {
+      const value = vl;
+      for (const val of newGrades) {
+        if (value.id === val.studentId) {
+          value.grade = val.grade;
+        }
+      }
+      if (typeof value.grade === 'undefined') {
+        value.grade = 'N/A';
+      }
+      return value;
     });
 }
