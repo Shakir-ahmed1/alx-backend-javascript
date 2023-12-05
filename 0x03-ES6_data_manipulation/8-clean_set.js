276fec974a7fc555b58288d6a850f23656295a7a
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-05 18:46:38 +0000

    fixing correct output 1

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,12 @@
 export default function cleanSet(set, startString) {
   const result = [];
+  if (startString === '') {
+    return '';
+  }
   for (const element of set) {
     if (element.startsWith(startString)) {
-      result.push(element.slice(startString.length));
+      // result.push(element.slice(startString.length));
+      result.push(element.substring(startString.length));
     }
   }
   return result.join('-');
