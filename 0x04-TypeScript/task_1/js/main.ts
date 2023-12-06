f2948eaeee48211de8ca5f8ee9190eac2d2b15e4
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-12-06 23:48:43 +0300

    printing teachers and writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// task 1
 interface Teacher {
     firstName: string;
     lastName: string;
@@ -17,3 +18,47 @@ const teacher3: Teacher = {
   
   console.log(teacher3);
   */
+
+
+// task 2
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+/*
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+  console.log(director1);
+*/
+
+//task 3
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+}
+/*
+console.log(printTeacher("Jhon", "Doe"));
+*/
+
+// task 4
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+}
+class StudentClass implements StudentClassInterface{
+    constructor(public firstName: string, public lastName: string) {}
+
+    workOnHomework(): string {
+        return "Currently working"
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
