16b7d88a9b0b071e377da8846ed39ccf015fb554
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-12-06 22:02:07 +0300

    adding task 1

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,11 @@
+/*
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+  };
+  
+  console.log(teacher3);
+  */ 
