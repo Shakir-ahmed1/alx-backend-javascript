e6cc1dbbdfae683219a4577f66d1a2268eadc2f9
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2024-01-30 17:13:42 +0300

    replacing var with const

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  var chunk = process.stdin.read();
+  const chunk = process.stdin.read();
   if (chunk !== null) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
 process.stdin.on('end', function() {
-  process.stdout.write('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
