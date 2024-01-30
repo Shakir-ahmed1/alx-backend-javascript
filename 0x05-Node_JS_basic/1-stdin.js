2ddd76475d44d99529d554b76415448b77401421
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2024-01-30 12:00:28 +0300

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,18 @@
-console.log('Welcome to Holberton School, what is your name?');
-const name = process.stdin.on('readable',()=> {
-    const nm = process.stdin.read();
-    if (nm !== null) {
-        const b = `${nm}`;
-        console.log(`Your name is: ${b.trim()}`);
-        console.log('This important software is now closing');
-    }
+const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+  console.log('This important software is now closing');
+  rl.close();
+});
+
+rl.on('close', () => {
+  process.exit(0);
 });
