80841f57004e7dc39e91fcea2f34491d5906887c
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-02-02 17:26:29 +0300

    resolving instead of raising error

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -17,32 +17,35 @@ function csvToList(csv) {
 }
 
 async function countStudents(path) {
-  const { stdout, stderr } = await exec(`cat ${path}`);
-
-  if (!stderr) {
-    const data = stdout;
-    const datals = csvToList(data.toString());
-    const fields = [];
-    for (const a of datals) {
-      if (!fields.includes(a[3])) {
-        fields.push(a[3]);
-      }
-    }
-    let result = `Number of students: ${datals.length}\n`;
-    for (const a of fields) {
-      let counter = 0;
-      const firstnames = [];
-      for (const b of datals) {
-        if (b[3] === a) {
-          counter += 1;
-          firstnames.push(b[0]);
+  try {
+    const { stdout, stderr } = await exec(`cat ${path}`);
+    if (!stderr) {
+      const data = stdout;
+      const datals = csvToList(data.toString());
+      const fields = [];
+      for (const a of datals) {
+        if (!fields.includes(a[3])) {
+          fields.push(a[3]);
         }
       }
-      result += `Number of students in ${a}: ${counter}. List: ${firstnames.join(', ')}\n`;
+      let result = `Number of students: ${datals.length}\n`;
+      for (const a of fields) {
+        let counter = 0;
+        const firstnames = [];
+        for (const b of datals) {
+          if (b[3] === a) {
+            counter += 1;
+            firstnames.push(b[0]);
+          }
+        }
+        result += `Number of students in ${a}: ${counter}. List: ${firstnames.join(', ')}\n`;
+      }
+      return { stdout: result.trim(), stderr };
     }
-    return { stdout: result.trim(), stderr };
+  } catch (e) {
+    return { stdout: 'Cannot load the database' };
   }
-  return { stdout: 'Cannot load the database', stderr };
+  return { stdout: 'success' };
 }
 
 const app = http.createServer(async (req, res) => {
