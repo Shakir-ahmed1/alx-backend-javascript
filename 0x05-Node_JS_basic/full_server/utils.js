d5a80bcd30e3985ae8ea87e995cbf8c91198f8fe
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-08 03:22:43 +0300

    update utils

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,25 +1,26 @@
 import fs from 'fs';
 
 export default function readDatabase(filePath) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            if (err) {
-                reject(err); // Reject with error if file is not accessible
-            } else {
-                // Parse the data and extract first names per fields
-                const lines = data.trim().split('\n');
-                const database = {};
-                lines.forEach((line, index) => {
-                    if (index !== 0) { // Skip header line
-                        const [firstName, lastName, age, field] = line.split(',');
-                        if (!database[field]) {
-                            database[field] = [];
-                        }
-                        database[field].push(firstName);
-                    }
-                });
-                resolve(database); // Resolve with the object of arrays of first names per fields
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err); // Reject with error if file is not accessible
+      } else {
+        // Parse the data and extract first names per fields
+        const lines = data.trim().split('\n');
+        const database = {};
+        lines.forEach((line, index) => {
+          if (index !== 0) { // Skip header line
+            // eslint-disable-next-line
+            const [firstName, lastName, age, field] = line.split(',');
+            if (!database[field]) {
+              database[field] = [];
             }
+            database[field].push(firstName);
+          }
         });
+        resolve(database); // Resolve with the object of arrays of first names per fields
+      }
     });
+  });
 }
