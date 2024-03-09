a45d2df9c272131e1e6e4711c21668411b2ee862
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-03-09 11:00:30 +0300

    fixing 500 status code message for students major

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -6,8 +6,8 @@ const app = express();
 app.use('/', routes);
 
 const PORT = 1245;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 export default app;
